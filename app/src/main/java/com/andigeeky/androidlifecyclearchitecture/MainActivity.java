68fce2bca736e861ad3d07e5ae0fdb5251d25101
Gelanee, Mamata <Mamata.Gelanee@mastercard.com>
2017-06-07 12:36:20 +0530

    created a basic demo

--- app/src/main/java/com/andigeeky/androidlifecyclearchitecture/MainActivity.java
+++ app/src/main/java/com/andigeeky/androidlifecyclearchitecture/MainActivity.java
@@ -1,13 +1,37 @@
 package com.andigeeky.androidlifecyclearchitecture;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import com.andigeeky.androidlifecyclearchitecture.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitName();
+            }
+        });
+    }
+
+    private void submitName() {
+        Editable name = activityMainBinding.edtName.getText();
+        if (name != null && !TextUtils.isEmpty(name.toString())) {
+            String msg = getString(R.string.lbl_txt_name) + " " + name.toString();
+            activityMainBinding.txtName.setText(msg);
+        } else {
+            Toast.makeText(this, R.string.msg_valid_name, Toast.LENGTH_SHORT).show();
+        }
     }
 }
